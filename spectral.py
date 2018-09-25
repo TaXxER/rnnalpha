2ed1986f6e3dd1ce450fb41d3dfb5294ad2fbf36
Niek Tax <niektax@gmail.com>
2018-09-25 14:32:13 +0200

    initial version of the spectral learning approach. implementation needs to be checked

--- spectral.py
+++ spectral.py
@@ -2,75 +2,50 @@ from splearn.spectral import Spectral
 from splearn.datasets.base import load_data_sample
 from sklearn.metrics import brier_score_loss
 from sklearn.model_selection import train_test_split
-import unicodecsv
 import numpy as np
-import random
-import sys
-import os
-import copy
-import csv
 
 X = load_data_sample('spice_logs/sepsis.spice')
 
-#random.shuffle(lines)
-
-#elems_per_fold = int(round(numlines/3))
-#fold1 = lines[:elems_per_fold]
-#with open('output_files/folds/fold1.csv', 'w') as csvfile:
-#    spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#    for row in fold1:
-#        spamwriter.writerow([str(s).encode("utf-8") for s in row])
-
-#fold2 = lines[elems_per_fold:2*elems_per_fold]
-#with open('output_files/folds/fold2.csv', 'w') as csvfile:
-#    spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#    for row in fold2:
-#        spamwriter.writerow([str(s).encode("utf-8") for s in row])
-        
-#fold3 = lines[2*elems_per_fold:]
-#with open('output_files/folds/fold3.csv', 'w') as csvfile:
-#    spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#    for row in fold3:
-#        spamwriter.writerow([str(s).encode("utf-8") for s in row])
-
-#lines = fold1 + fold2
-X_train, X_test = train_test_split(X, test_size=0.33)
+X_train, X_test = train_test_split(X.data, test_size=0.33)
 # build the model: 
 print('Build model...')
-model = Spectral()
-# train the model, output generated text after each iteration
-model.fit(X.data)
-
-# test model
-#sentences = []
-#next_chars = []
-#for line in fold3:
-#    for i in range(0, len(line), step):
-#        sentences.append(line[0: i])
-#        next_chars.append(line[i])
-
-#X_test = np.zeros((len(sentences), maxlen, num_features), dtype=np.int32)
-#y_test = np.zeros((len(sentences), len(target_chars)), dtype=np.float32)
-#y_hat = np.empty((len(target_chars)), dtype=np.float32)
-#for i, sentence in enumerate(sentences):
-#    if(len(sentence)>maxlen):
-#        sentence = sentence[0:maxlen] 
-#    leftpad = maxlen-len(sentence)
-#    for t, char in enumerate(sentence):
-#        for c in chars:
-#            if c==char:
-#                X_test[i, t+leftpad, char_indices[c]] = 1
-#        X_test[i, t+leftpad, len(chars)] = t+1
-
-    #if next_chars[i]=='}':
-    #    continue
-#    for c in target_chars:
-#        if c==next_chars[i]:
-#            y_test[i, char_indices[c]] = 1-softness
-#        else:
-#            y_test[i, char_indices[c]] = softness/(len(target_chars)-1)
-    #y_hat = np.vstack((y_hat,model.predict(X[i].reshape((1,maxlen,num_features)))))
-
-#y_hat = model.predict_proba(X_test)
-
-#print(np.mean(list(map(lambda x: brier_score_loss(y_test[x],y_hat[x]),[i[0] for i in enumerate(y_test)]))))
+model = Spectral(full_svd_calculation=True)
+model.fit(X_train)
+# model._automaton.val calculates probabilities over complete sequences
+# Ap.val calculates probabilities over prefixes
+Ap = model._automaton.transformation(source = "classic", target = "prefix")
+brier_scores = []
+for test_seq in X_test:
+    test_seq = [int(x) for x in test_seq]
+    prefix = []
+    for i, elem in enumerate(test_seq): # elem \in \{-1\}\union [1,|activities|]
+        # construct prediction vector for prefix
+        y_hat = []
+        for a in list(range(X.nbL)):
+            #if a==0: # a \in [0,|activities], therefore, by mapping 0 to -1, a maps on elem.
+            #    a = -1 
+            candidate = prefix + [a]
+            y_hat += [max(0, Ap.val(candidate))]
+            #print(max(0, Ap.val(candidate)))
+        # add probability of sequence end to y_hat
+        y_hat += [max(0, model._automaton.val(prefix))]
+        #y_hat += [0]
+        divisor = sum(y_hat)
+        if divisor == 0:
+            divisor = 1
+        y_hat = [x/divisor for x in y_hat]
+        # construct ground truth vector for prefix
+        y = np.zeros(X.nbL+1)
+        if elem==-1:
+            y[elem] = 1
+        else:
+            y[elem-1] = 1 # -1 to convert [1,activities] to [0,activities-1]
+        brier_scores += [brier_score_loss(y,y_hat)]
+        #print(y)
+        #print(y_hat)
+        #print(brier_score_loss(y,y_hat))
+        #print()
+        prefix += [elem-1]
+        if elem == -1: # stop making predictions for this sequence after end symbol is observed 
+            break
+print(np.sum(brier_scores)/len(brier_scores))
